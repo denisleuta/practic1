774c2b4f80ed1b73001f177574e543ca8129e62a
denis <denis@DESKTOP-8AJ8D96>
2024-11-22 23:33:07 +0300

    Update README and returned everything to one file

--- normalization.py
+++
@@ -1,8 +0,0 @@
-from sklearn.preprocessing import MinMaxScaler
-import pandas as pd
-
-
-def normalize_data(data, features):
-    scaler = MinMaxScaler()
-    normalized = pd.DataFrame(scaler.fit_transform(data[features]), columns=features)
-    return normalized
